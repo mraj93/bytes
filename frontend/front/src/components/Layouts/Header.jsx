ef4eee61e6f0f3d205d78ff46431ad7cf680a35c
Mukesh's Linux <mrajbhar009@gmail.com>
2023-11-24 18:22:31 +0530

    header modifications

--- frontend/front/src/components/Layouts/Header.jsx
+++ frontend/front/src/components/Layouts/Header.jsx
@@ -1,41 +1,47 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import '../../CSS/header.css';
-import { FaChartLine, FaSearch, FaBell, FaBars } from 'react-icons/fa'; // Import icons from react-icon
+import { FaChartLine, FaSearch, FaBell, FaBars} from 'react-icons/fa'; // Import icons from react-icon
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBullseye } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
   const [isMarketsSelected, setIsMarketsSelected] = useState(false);
   
+  let percenatge = 0.31;
+  
   return (
     <header className="header">
       <div className="header-container">
+        
         <div className="link-container">
           <Link to={"/"} className="logo-text mx-5">Bytes</Link>
-          <Link to={"/"} className="header-link"><FaChartLine /></Link>s
+          <Link to={"/"} className="header-link"><FaChartLine /></Link>
           <Link to={"/"} className="header-link">Buy crypto</Link>
           
           <div className="hover-options">
-          <Link
-          to={"/"}
-          className={`header-link ${isMarketsSelected ? 'selected' : ''}`}
-          onMouseEnter={() => setIsMarketsSelected(true)}
-          onMouseLeave={() => setIsMarketsSelected(false)}
-        >
-        Markets
-      </Link>
+          <Link to={"/"} 
+                className={`header-link ${isMarketsSelected ? 'selected' : ''}`} 
+                onMouseEnter={() => setIsMarketsSelected(true)} 
+                onMouseLeave={() => setIsMarketsSelected(false)}>Markets</Link>
+            
             <div className="options">
-              <Link to={"/abc"}>abc</Link>
-              <Link to={"/abc"}>xyz</Link>
-              <Link to={"/abc"}>yzx</Link>
-              <Link to={"/abc"}>mln</Link>
+              <p className="mx-4">Market is up by {percenatge} in the last 24 hours.</p>
+              {/* <p></p> */}
+              <FontAwesomeIcon icon={faBullseye} /><Link to={"/"} className="header-link mx-2">Trade1</Link>
+              <Link to={"/"} className="header-link mx-2">Trade2</Link>
+              <Link to={"/"} className="header-link mx-2">Trade3</Link>
             </div>
-          </div>
+            </div>
+          
           <Link to={"/"} className="header-link">Trade</Link>
-          <Link to={"/"} className="header-link">Copy1</Link>
+          <Link to={"/"} className="header-link">Copy</Link>
           <Link to={"/"} className="header-link">Bots</Link>
           <Link to={"/"} className="header-link">Earn</Link>
           <Link to={"/"} className="header-link">Web3</Link>
+s
         </div>
+        
         <div className="right-side">
           <Link to={"/"} className="header-link">Log in</Link>
           <button className='custom-btn'>Sign Up</button>
