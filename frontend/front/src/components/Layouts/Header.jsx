1133109d9f2ff4594f635308f56628170d4ca8f1
Mukesh's Linux <mrajbhar009@gmail.com>
2023-11-24 11:58:13 +0530

    testing purpose

--- frontend/front/src/components/Layouts/Header.jsx
+++ frontend/front/src/components/Layouts/Header.jsx
@@ -1,18 +1,37 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import '../../CSS/header.css';
 import { FaChartLine, FaSearch, FaBell, FaBars } from 'react-icons/fa'; // Import icons from react-icon
 
 const Header = () => {
+  const [isMarketsSelected, setIsMarketsSelected] = useState(false);
+  
   return (
     <header className="header">
       <div className="header-container">
         <div className="link-container">
           <Link to={"/"} className="logo-text mx-5">Bytes</Link>
-          <Link to={"/"} className="header-link"><FaChartLine /></Link>
+          <Link to={"/"} className="header-link"><FaChartLine /></Link>s
           <Link to={"/"} className="header-link">Buy crypto</Link>
-          <Link to={"/"} className="header-link">Markets</Link>
+          
+          <div className="hover-options">
+          <Link
+          to={"/"}
+          className={`header-link ${isMarketsSelected ? 'selected' : ''}`}
+          onMouseEnter={() => setIsMarketsSelected(true)}
+          onMouseLeave={() => setIsMarketsSelected(false)}
+        >
+        Markets
+      </Link>
+            <div className="options">
+              <Link to={"/abc"}>abc</Link>
+              <Link to={"/abc"}>xyz</Link>
+              <Link to={"/abc"}>yzx</Link>
+              <Link to={"/abc"}>mln</Link>
+            </div>
+          </div>
           <Link to={"/"} className="header-link">Trade</Link>
-          <Link to={"/"} className="header-link">Copy</Link>
+          <Link to={"/"} className="header-link">Copy1</Link>
           <Link to={"/"} className="header-link">Bots</Link>
           <Link to={"/"} className="header-link">Earn</Link>
           <Link to={"/"} className="header-link">Web3</Link>
